a04b1c29869aed762e446333f2bb9c4b9d4ae9fc
Ivan <OtradinskiyIvan>
2026-07-20 18:30:39 +0300

    add temp redirect for archive

--- mysite/catalog/views.py
+++ mysite/catalog/views.py
@@ -1,4 +1,8 @@
-from django.http import HttpRequest, HttpResponse, HttpResponseNotFound
+from django.http import HttpRequest, HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.shortcuts import redirect
+import datetime
+
+from django.urls import reverse
 
 
 def index(request: HttpRequest) -> HttpResponse:
@@ -10,7 +14,11 @@ def category_by_id(request: HttpRequest, cat_id: int) -> HttpResponse:
 def category_by_slug(request: HttpRequest, cat_slug: str) -> HttpResponse:
     return HttpResponse(f'<h1>Categories</h1><p>cat slug: {cat_slug}<p/>')
 
-def archive(request: HttpRequest, year: int) -> HttpResponse:
+def archive(request: HttpRequest, year: int) -> HttpResponse | HttpResponseRedirect:
+    if year > datetime.datetime.now().year:
+        uri = reverse('catalog_main')
+        return HttpResponseRedirect(uri)
+    
     return HttpResponse(f'<h1>Archive</h1><p>Year: {year}<p/>')
 
 def page_not_found(request: HttpRequest, exception) -> HttpResponseNotFound:
