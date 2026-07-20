03d00edb8ea2e6b8f2254af376fb5d859b0f32bb
Ivan <OtradinskiyIvan>
2026-07-20 17:36:24 +0300

    add new url paths and views

--- mysite/catalog/urls.py
+++ mysite/catalog/urls.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
-from django.urls import path
-from .views import index, categories
+from django.urls import path, register_converter
+from .views import index, category_by_id, category_by_slug, archive
+from .converters import YearConverter
 
+register_converter(YearConverter, 'year_conv')
 
 urlpatterns = [
     path('', index),
-    path('cats/', categories),
+    path('cats/<int:cat_id>/', category_by_id),
+    path('cats/<slug:cat_slug>/', category_by_slug),
+    path('archive/<year_conv:year>/', archive),
 ]
--- mysite/catalog/views.py
+++ mysite/catalog/views.py
@@ -1,9 +1,14 @@
-from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
 
 
 def index(request: HttpRequest) -> HttpResponse:
     return HttpResponse('Catalog main page')
 
-def categories(request: HttpRequest) -> HttpResponse:
-    return HttpResponse('<h1>Categories</h1>')
+def category_by_id(request: HttpRequest, cat_id: int) -> HttpResponse:
+    return HttpResponse(f'<h1>Categories</h1><p>cat id: {cat_id}<p/>')
+
+def category_by_slug(request: HttpRequest, cat_slug: str) -> HttpResponse:
+    return HttpResponse(f'<h1>Categories</h1><p>cat slug: {cat_slug}<p/>')
+
+def archive(request: HttpRequest, year: int) -> HttpResponse:
+    return HttpResponse(f'<h1>Archive</h1><p>Year: {year}<p/>')
