344165ec9bd13b5214acc6c8a8ca12d5f26b2f58
Ivan <OtradinskiyIvan>
2026-07-30 21:38:05 +0300

    dev: removed initial secrets from settings.

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,11 +21,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-vjtku0*au$ytv0yrf_fmc*5n+5pugnwrsjh8dcus9+iu0l#(ju'
+load_dotenv()
+
+SECRET_KEY = os.getenv('SECRET_KEY')
+if not SECRET_KEY:
+    raise ValueError('Failed to load secret key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.getenv('DEBUG')
+if not DEBUG:
+    raise ValueError('Failed to load debug mode val')
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
