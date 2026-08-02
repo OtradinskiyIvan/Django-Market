0b680cd1bf603df7d722c2794d644efcd11ae1e8
Ivan <OtradinskiyIvan>
2026-08-02 19:35:18 +0300

    feat: add forms for login, add item and contact.

---
+++ mysite/catalog/forms.py
@@ -0,0 +1,24 @@
+from django import forms
+from .models import *
+
+
+class AddItemForm(forms.Form):
+    title = forms.CharField(max_length=255)
+    slug = forms.SlugField(max_length=255, label='URL')
+    seller_id = forms.IntegerField()
+    price = forms.IntegerField()
+    desc = forms.CharField(widget=forms.Textarea())
+    is_available = forms.BooleanField(initial=True, required=False)
+    category = forms.ModelChoiceField(queryset=Category.objects.all(), empty_label='Set category')
+    tags = forms.ModelChoiceField(queryset=Tag.objects.all(), empty_label='Set tag')
+
+
+class LoginForm(forms.Form):
+    name = forms.CharField(max_length=255)
+    email = forms.EmailField()
+    phone = forms.CharField(required=False)
+    role = forms.ChoiceField(choices=Profile.Role.choices)
+
+
+class ContactForm(forms.Form):
+    message = forms.CharField(widget=forms.Textarea())
