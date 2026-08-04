7f40d86903b6c9d032df94ce22ff4760844c8ad6
Ivan <OtradinskiyIvan>
2026-08-04 20:02:09 +0300

    feat: add phone nu,ber validator. add password confirmation.

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'catalog.apps.CatalogConfig',
     'users.apps.UsersConfig',
+    'phonenumber_field',
 ]
 
 MIDDLEWARE = [
--- mysite/users/forms.py
+++ mysite/users/forms.py
@@ -3,13 +3,24 @@ from .models import Profile
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import get_user_model
 
+from phonenumber_field.formfields import PhoneNumberField
+
 
 class AuthForm(AuthenticationForm):
     name = forms.CharField(max_length=255)
     email = forms.EmailField()
-    phone = forms.CharField(required=False)
+    phone = PhoneNumberField()
     role = forms.ChoiceField(choices=Profile.Role.choices)
     password = forms.CharField(widget=forms.PasswordInput())
+    password_conf = forms.CharField(widget=forms.PasswordInput())
+
+    def clean(self):
+        clean_data = super().clean()
+        password = clean_data.get('password')
+        password_conf = clean_data.get('password_conf')
+
+        if password and password_conf and password != password_conf:
+            raise forms.ValidationError("Passwords didnt match")
 
 
 class LoginForm(AuthenticationForm):
