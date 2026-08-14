89818b4133c08c85f45404806fd9257cd8393d1a
Ivan <OtradinskiyIvan>
2026-08-14 22:45:29 +0300

    feat: add login url and email env

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -54,6 +54,17 @@ ALLOWED_HOSTS = ['127.0.0.1']
 
 LOGOUT_REDIRECT_URL = 'login'
 
+LOGIN_URL = 'login'
+
+# email env
+
+EMAIL_BACKEND = os.getenv('EMAIL_BACKEND', 'django.core.mail.backends.console.EmailBackend')
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_PORT = int(os.getenv('EMAIL_PORT', 587))
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
 
 # Application definition
 
