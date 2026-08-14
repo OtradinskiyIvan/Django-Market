42d4c69ff6e58901c382a8a406435a6be28013db
Ivan <OtradinskiyIvan>
2026-08-14 22:35:15 +0300

    feat: add model for SellerReview, add balance and favorites fields to Profile.

--- mysite/users/models.py
+++ mysite/users/models.py
@@ -1,3 +1,4 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -13,8 +14,21 @@ class Profile(models.Model):
     phone = models.CharField(max_length=255)
     role = models.IntegerField(choices=Role.choices, default=Role.CUSTOMER)
     is_verified = models.BooleanField(default=False)
+    balance = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    favorites = models.ManyToManyField('catalog.Catalog', blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
           return f"{self.get_role_display()} {self.name}"
+
+
+class SellerReview(models.Model):
+     customer = models.ForeignKey('users.Profile', on_delete=models.SET_NULL, related_name='review_given', null=True)
+     seller = models.ForeignKey('users.Profile', on_delete=models.SET_NULL, related_name='review_received', null=True)
+     rating = models.PositiveSmallIntegerField(
+          validators=[MinValueValidator(1), MaxValueValidator(5)],
+     )
+     created_at = models.DateTimeField(auto_now_add=True)
+     comment = models.TextField(blank=True)
+     updated_at = models.DateTimeField(auto_now=True)
