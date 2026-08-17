aefb21973eafc546ac15064545cd896d030dd7ce
Ivan <OtradinskiyIvan>
2026-08-18 01:29:09 +0300

    feat: add password chenge

--- mysite/users/urls.py
+++ mysite/users/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(), name='logout'),
     path('register/', RegisterUser.as_view(), name='register'),
     path('profile/', ProfileUser.as_view(), name='profile'),
+path('password-change/', PasswordChangeView.as_view(
+    template_name='users/password_change_form.html'), name='password_change'),
+path('password-change/done/', PasswordChangeDoneView.as_view(
+    template_name='users/password_change_done.html'), name='password_change_done'),
 ]
