4a68e4e51710527e7b12c6bf32da913d618f7ec8
Ivan <OtradinskiyIvan>
2026-08-18 03:11:03 +0300

    feat: add password reset via email

--- mysite/users/urls.py
+++ mysite/users/urls.py
@@ -1,5 +1,9 @@
-from django.urls import path
-from django.contrib.auth.views import LogoutView, PasswordChangeView, PasswordChangeDoneView
+from django.urls import path, reverse_lazy
+from django.contrib.auth.views import (
+    LogoutView, PasswordChangeView, PasswordChangeDoneView, 
+    PasswordResetView, PasswordResetDoneView,
+    PasswordResetConfirmView, PasswordResetCompleteView
+)
 from .views import *
 
 urlpatterns = [
@@ -11,4 +15,17 @@ path('password-change/', PasswordChangeView.as_view(
     template_name='users/password_change_form.html'), name='password_change'),
 path('password-change/done/', PasswordChangeDoneView.as_view(
     template_name='users/password_change_done.html'), name='password_change_done'),
+path('password-reset/done/', PasswordResetDoneView.as_view(
+    template_name='users/password_reset_done.html'), name='password_reset_done'),
+path('password-reset/<uidb64>/<token>/', PasswordResetConfirmView.as_view(
+    template_name='users/password_reset_confirm.html',
+    success_url=reverse_lazy('password_reset_complete'),
+    ), name='password_reset_confirm'),
+path('password-reset/complete/', PasswordResetCompleteView.as_view(
+    template_name='users/password_reset_complete.html'), name='password_reset_complete'),
+path('password-reset/', PasswordResetView.as_view(
+    template_name='users/password_reset_form.html',
+    email_template_name='users/password_reset_email.html',
+    success_url=reverse_lazy('password_reset_done'),
+), name='password_reset'),
 ]
